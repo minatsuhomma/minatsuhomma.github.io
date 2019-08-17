4860188d4cb8b8a8a99243cc4f07aecb3b009efd
minatsuhomma <54023892+minatsuhomma@users.noreply.github.com>
2019-08-17 23:35:05 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,13 +2,31 @@ $(window).on('load resize', function(){
   // navbarの高さを取得し、bodyのpaddingにnavbarの高さを設定する
   let height = $('.navbar').height();
   $('body').css('padding-top',height); 
-  
-  const category = document.getElementsByClassName('filter');
 
-  $(".filter").on("click",function(){
-    if (category[0].innerText == "UI/UX") {
+  //isotopeライブラリの初期化
+  var grid = $('.pj-container').isotope({
+    itemSelector: '.project',
+    layoutMode: 'fitRows'
+  });
+
+  const category = document.getElementsByClassName('filter');
+  $(".filter").on("click",function(e){
+    let target = e.currentTarget;
+    if(target.innerText == "Show All") {
+      console.log("all");
+      grid.isotope({ filter: '*' });
+    }else if(target.innerText == "UI/UX") {
       console.log("uiux");
-      $(".project").filter(".uiux").show();
-    }   
+      grid.isotope({ filter: '.uiux' });
+    }else if(target.innerText == "Physical Computing"){
+      console.log("physical");
+      grid.isotope({ filter: '.physical' });
+    }else if(target.innerText == "Machine Learning"){
+      console.log("ml");
+      grid.isotope({ filter: '.ml' });
+    }else if(target.innerText == "Design Concept"){
+      console.log("concept");
+      grid.isotope({ filter: '.concept' });
+    }
   }) 
 });
